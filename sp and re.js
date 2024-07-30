70c5c6c5de33b319fc34e85162c45b91216f318d
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-07-30 11:53:23 +0530

    spread and rest operator works

---
+++ sp and re.js
@@ -0,0 +1,54 @@
+// schoolAStudents = ['Anto','Franklin'];
+// schoolBStudents = ['Jasmine','Sam'];
+
+
+// students = schoolAStudents.concat(schoolBStudents);
+
+
+// students = [...schoolAStudents,...schoolBStudents,'Max'];
+// console.log(students);
+
+
+// studentName = {
+//     firstName:'Franklin',
+//     lastName:'Anto'
+// }
+
+// studentDetails = {
+//     age:30,
+//     gender:'M',
+//     email:'anto@gmail.com'
+// }
+
+
+// student = {...studentName,...studentDetails,country:'india'};
+
+// console.log(student);
+
+
+// studentName = 'Franklin';
+// ans = [...studentName];
+
+// console.log(ans);
+
+
+// function sum(...inputValue){
+
+//     ans = 0;
+//     for(i=0;i<inputValue.length;i++){
+//         ans = ans+inputValue[i]
+//     }
+//     console.log(ans);
+// }
+
+
+
+// sum(1,7,6,4)
+
+
+numbers = [2,3,4,1,5,0.2]
+
+const ans = Math.min(...numbers)
+
+console.log(ans);
+ 
